b573163d6fdb497481b06a2222caa26059fef40f
Keith Pilson <email": null>
2016-08-10 15:33:18 -0500

    created the link to the new question page but cannot complete the logic until the login page is complete

--- app/controllers/questions.rb
+++ app/controllers/questions.rb
@@ -3,8 +3,30 @@ get '/questions' do
   erb :index
 end
 
+get '/questions/new' do
+  # if user = User.find(session[:user_id])
+    erb :"/questions/new"
+  # else
+  #   redirect '/login'
+  # end
+end
+
+post '/questions' do
+  @question = Question.new(params[:question], user_id: session[:user_id])
+  # @question(user_id: session[:user_id])
+  if @question.save
+    redirect "/questions"
+  else
+    @errors = @question.errors.full_messages
+    erb :'/questions/new'
+  end
+end
+
 get '/questions/:question_id' do
   @question = Question.find(params[:question_id])
   @answers = Answer.where(question_id: @question.id)
   erb :'/questions/show'
 end
+
+
+
