24370110fc5b94b55281cebbf0d09590d647c84f
Derick Manliclic & Jacob Golding <pair+dmanliclic715.jcbgldng@devbootcamp.com>
2016-08-11 10:25:05 -0500

    create basic comment outline

--- app/controllers/questions.rb
+++ app/controllers/questions.rb
@@ -4,11 +4,12 @@ get '/questions' do
 end
 
 get '/questions/new' do
-  # if user = User.find(session[:user_id])
+  user = self.current_user
+  if logged_in?
     erb :"/questions/new"
-  # else
-  #   redirect '/login'
-  # end
+  else
+    redirect '/login'
+  end
 end
 
 post '/questions' do
@@ -26,6 +27,21 @@ get '/questions/:question_id' do
   @question = Question.find(params[:question_id])
   @answers = Answer.where(question_id: @question.id)
   erb :'/questions/show'
+end
+
+post '/questions/:question_id/comments' do
+  question = Question.find(params[:commentable])
+  puts '----------------------------'
+  comment = question.comments.new(params[:comment])
+  comment.user_id = session[:user_id]
+  comment
+  if question.save
+  else
+    @errors = @comment.errors.full_messages
+  end
+  @question = Question.find(params[:commentable])
+  @answers = Answer.where(question_id: @question.id)
+  erb :"/questions/show"
 end
 
 
