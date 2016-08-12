04af976f7ae40800f59e066a9b6a58b1a246f28a
dmanliclic715 <dmanliclic715@gmail.com>
2016-08-11 19:07:06 -0500

    js stuff

--- public/js/application.js
+++ public/js/application.js
@@ -1,17 +1,3 @@
 $(document).ready(function() {
 
-  $('.vote-buttons-container').on("click", ".vote-button", function(e){
-    e.preventDefault();
-    var $form = $(this).parent();
-    var path = $form.serialize();
-    console.log($(this).attr('value'));
-
-  //   $.ajax({
-  //     url: $form.attr('action'),
-  //     type: $form.attr('method'),
-  //     data: {vote: postId},
-  //     dataType: 'json'
-  //   })
-  })
-
 })
